1148f4dfd8656a144b31e2172a53bf6c66b91af1
KathyPuma <kathypuma96@gmail.com>
2020-04-05 18:45:17 -0400

    Delete route to get all customers

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -2,23 +2,11 @@ const express = require('express');
 const router = express.Router();
 const customerQueries = require('../queries/customersQueries')
 
-router.get('/', async(req, res, next) =>{
-    try{
-        let customers = await customerQueries.getAllCustomers()
-        res.json({
-            status: "success",
-            message: 'All customers recieved',
-            payload: customers
-        })
-    }catch(err){
-        console.log("ERROR", err);
-    }
-})
 
 
 router.post("/register", async (req, res, next) => {
     try {
-        const { firstname, lastname, phone_number, email, address, city, state, zip_code, avatar_url, password} = req.body
+        const { firstname, lastname, phone_number, email, address, city, state, zip_code, avatar_url, password } = req.body
         const response = await customerQueries.addCustomer({ firstname, lastname, phone_number, email, address, city, state, zip_code, avatar_url, password });
         res.json({
             status: "success",
@@ -49,7 +37,7 @@ router.get("/:id", async (req, res, next) => {
 router.patch("/edit/:id", async (req, res, next) => {
     try {
         const customer_id = req.params.id
-        const { firstname, lastname, phone_number, email, address, city, state, zip_code, avatar_url, password} = req.body
+        const { firstname, lastname, phone_number, email, address, city, state, zip_code, avatar_url, password } = req.body
 
         const updatedInfo = await customerQueries.updateCustomerInfo({ customer_id, firstname, lastname, phone_number, email, address, city, state, zip_code, avatar_url, password });
         res.json({
