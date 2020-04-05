c2dd53c57b0e63bed49d4f1d90afa0c3203db304
KathyPuma <kathypuma96@gmail.com>
2020-04-05 13:37:51 -0400

    customers routes and queries to register, delete, edit profile, get info by id

--- server/queries/customersQueries.js
+++ server/queries/customersQueries.js
@@ -1,4 +1,4 @@
-const db = require ('../database/db')
+const db = require('../database/db')
 
 const getAllCustomers = async () =>{
     const getAllQueries = `
@@ -7,6 +7,98 @@ const getAllCustomers = async () =>{
     return await db.any(getAllQueries)
 }
 
+
+const addCustomer = async (bodyObj) => {
+    const postQuery = `
+        INSERT INTO customers (
+            firstname,
+            lastname,
+            phone_number,
+            email,
+            address,
+            city,
+            state,
+            zip_code,
+            avatar_url,
+            password
+        )
+        VALUES (
+            $/firstname/,
+            $/lastname/,
+            $/phone_number/,
+            $/email/,
+            $/address/,
+            $/city/,
+            $/state/,
+            $/zip_code/,
+            $/avatar_url/,
+            $/password/
+        )
+        RETURNING 
+            customer_id,
+            firstname,
+            lastname,
+            phone_number,
+            email,
+            address,
+            city,
+            state,
+            zip_code,
+            avatar_url
+      `;
+    return await db.one(postQuery, bodyObj);
+}
+
+
+const getCustomerById = async (id) => {
+    const getQuery = `
+    SELECT  firstname,
+            lastname,
+            phone_number,
+            email,address,
+            city,
+            state,
+            zip_code,
+            avatar_url
+        FROM customers
+        WHERE customer_id = $/id/;
+      `;
+    return await db.one(getQuery, { id });
+}
+
+
+const updateCustomerInfo = async (updateObj) => {
+    let updateQuery = `
+        UPDATE customers
+        SET firstname = $/firstname/,
+            lastname = $/lastname/,
+            phone_number = $/phone_number/,
+            email = $/email/,
+            address = $/address/,
+            city = $/city/,
+            state = $/state/,
+            zip_code = $/zip_code/,
+            avatar_url = $/avatar_url/,
+            password = $/password/
+            WHERE customer_id = $/customer_id /
+            RETURNING *
+        ;
+    `;
+    return await db.one(updateQuery, updateObj);
+}
+
+
+const deleteCustomer = async (id) => {
+    const deleteQuery = `DELETE FROM customers
+    WHERE customer_id = $/id/`;
+    return await db.none(deleteQuery, { id });
+};
+
+
 module.exports = {
-    getAllCustomers
+    getAllCustomers,
+    addCustomer,
+    getCustomerById,
+    updateCustomerInfo,
+    deleteCustomer
 };
--- server/routes/customers.js
+++ server/routes/customers.js
@@ -16,5 +16,96 @@ router.get('/', async(req, res, next) =>{
 })
 
 
+router.post("/register", async (req, res, next) => {
+    try {
+        const firstname = req.body.firstname
+        const lastname = req.body.lastname
+        const phone_number = req.body.phone_number
+        const email = req.body.email
+        const address = req.body.address
+        const city = req.body.city
+        const state = req.body.state
+        const zip_code = req.body.zip_code
+        const avatar_url = req.body.avatar_url
+        const password = req.body.password
+
+        const response = await customerQueries.addCustomer({
+            firstname,
+            lastname,
+            phone_number,
+            email,
+            address,
+            city,
+            state,
+            zip_code,
+            avatar_url,
+            password,
+        });
+        res.json({
+            status: "success",
+            message: `${firstname} ${lastname} successfully registered`,
+            payload: response
+        });
+    } catch (err) {
+        console.log("ERROR", err)
+    }
+});
+
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id)
+        const customerById = await customerQueries.getCustomerById(id);
+        res.json({
+            status: "success",
+            message: `Customer ${id} retrieved`,
+            payload: customerById
+        });
+    } catch (err) {
+        console.log("ERROR", err)
+    }
+});
+
+
+router.patch("/edit/:id", async (req, res, next) => {
+    try {
+        const customer_id = req.params.id
+        const firstname = req.body.firstname
+        const lastname = req.body.lastname
+        const phone_number = req.body.phone_number
+        const email = req.body.email
+        const address = req.body.address
+        const city = req.body.city
+        const state = req.body.state
+        const zip_code = req.body.zip_code
+        const avatar_url = req.body.avatar_url
+        const password = req.body.password
+
+        const updatedInfo = await customerQueries.updateCustomerInfo({ customer_id, firstname, lastname, phone_number, email, address, city, state, zip_code, avatar_url, password });
+        res.json({
+            status: "success",
+            message: `customers profile was edited`,
+            payload: updatedInfo
+        });
+    } catch (err) {
+        console.log("ERROR", err)
+    }
+});
+
+
+router.delete("/delete/:id", async (req, res, next) => {
+    try {
+        const customer_id = parseInt(req.params.id);
+        const deletedCustomer = await customerQueries.deleteCustomer(customer_id);
+        res.json({
+            status: "success",
+            message: `Customer ${customer_id} deleted`,
+            payload: deletedCustomer
+        });
+    } catch (err) {
+        console.log("ERROR", err)
+    }
+});
+
 
 module.exports = router;
