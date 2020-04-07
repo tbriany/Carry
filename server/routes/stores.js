a55c2e99432aa8aac7c0e52162e6bb99c66eb86c
aransagarcia <aransagarcia@pursuit.org>
2020-04-07 12:26:01 -0400

    fix router status message

--- server/routes/stores.js
+++ server/routes/stores.js
@@ -12,7 +12,7 @@ router.get('/', async(req, res, next) =>{
             payload: allStores
         })
     }catch(err){
-        console.log("ERROR retrieving all stores", err);
+        console.log("ERROR", err);
     }
 })
 
@@ -28,7 +28,7 @@ router.get("/:id", async (req, res, next) => {
         });
     } catch (err) {
         console.log("ERROR", err)
-    }
+        }
 });
 
 router.post("/register", async (req, res, next) => {
@@ -39,11 +39,11 @@ router.post("/register", async (req, res, next) => {
 
         res.status(200).json({
             status: "success",
-            message: `Store ${id} successfully registered`,
+            message: `Store ${response.store_id} successfully registered`,
             payload: response
         });
     } catch (err) {
-        console.log("ERROR: Unable to register store.", err)
+        console.log("ERROR", err)
     }
 });
 
@@ -55,8 +55,8 @@ router.patch("/edit/:id", async (req, res, next) => {
         const updatedInfo = await storesQueries.updateStoreInfo({ store_id, store_name, avatar_url, phone_number, email, address, city, state, zip_code, password });
 
         res.status(200).json({
-            status: "Store information successfully updated",
-            message: `Store ${id} profile was edited`,
+            status: "success",
+            message: `Store ${store_id} profile was edited`,
             payload: updatedInfo
         });
     } catch (err) {
@@ -70,7 +70,7 @@ router.delete("/delete/:id", async (req, res, next) =>{
         const deletedStore = await storesQueries.deleteStore(store_id)
 
         res.status(200).json({
-            status: 'success. Store Deleted', 
+            status: 'success', 
             message: `store ${store_id} deleted`,
             payload: deletedStore
         })
