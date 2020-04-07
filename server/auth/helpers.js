4b5930ea86087b5bd2c1fccf7f1ed3e94ca581b4
gsanchez13 <gisellesanchez@pursuit.org>
2020-04-07 13:14:00 -0400

    adds helpers functions to auth folders

---
+++ server/auth/helpers.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = async(password) => {
+    try {
+        const salt = await bcrypt.genSalt(12);
+        const password_digest = await bcrypt.hash(password, salt);
+        return password_digest;
+    }
+    catch(err) {
+        throw err;
+    }
+};
+
+const comparePassword = async (candidatePassword, passwordDigest) => {
+    try {
+        const match = await bcrypt.compare(candidatePassword, passwordDigest);
+        return match;
+    }
+    catch(err){
+        throw err;
+    }
+};
+
+const loginRequired = (req, res, next) => {
+    if(req.user) {
+        return next();
+    }
+    else {
+        res.status(401)
+        .json({
+            payload: null,
+            message: "You must be logged in to access Carry",
+            error: true,
+        })
+    }
+};
+
+module.export = {
+    hashPassword,
+    comparePassword,
+    loginRequired
+};
