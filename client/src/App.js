2e7a83d02f17b5b3b5d641a286448f3bb241476f
gsanchez13 <gisellesanchez@pursuit.org>
2020-04-14 18:39:57 -0400

    adds checkout component to app page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import './App.css';
-
+import Checkout from './Components/Checkout';
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Route path="/checkout" component={Checkout}/>
     </div>
   );
 }
