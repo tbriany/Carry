256bbdc77241f8a6479be0b8c41396cd2c062d4a
tbriany <brianytaveras@pursuit.org>
2020-04-14 22:11:15 -0400

    creates Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import './App.css';
 
-
+import Navbar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
     </div>
   );
 }
