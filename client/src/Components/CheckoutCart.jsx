5667471b375a083421ddfdc85077b11c8f161a46
KathyPuma <kathypuma96@gmail.com>
2020-04-19 13:06:32 -0400

    Add very little CSS

--- client/src/Components/CheckoutCart.jsx
+++ client/src/Components/CheckoutCart.jsx
@@ -2,16 +2,36 @@
 import React, { useState, useEffect, useContext, useRef } from 'react'
 import { Button, InputLabel, MenuItem, Select } from '@material-ui/core'
 import { ItemDetailsContext } from '../Contexts/ItemDetailsContexts'
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
 import axios from 'axios'
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1
+    },
+    paper: {
+        padding: theme.spacing(5),
+        textAlign: "center",
+        color: theme.palette.text.secondary
+    }
+}));
+
+
 const CheckoutCart = () => {
-    const {productId,  productIds, productQty, totalProductQty } = useContext(ItemDetailsContext)    //Acts like ItemDetailsContext.consumer but allows the entire CheckoutCart.jsx access to the state from Contexts/ItemDeatilsContext.js. 
+
+
+
+    const classes = useStyles();
+
+    const { productId, productIds, productQty, totalProductQty } = useContext(ItemDetailsContext)    //Acts like ItemDetailsContext.consumer but allows the entire CheckoutCart.jsx access to the state from Contexts/ItemDeatilsContext.js. 
     // ItemDetailsContext.consumer is found in the return and wraps around html tags (div, p, h1 etc.). It will only give those specific tags access to the state
 
 
     const [checkoutCart, setCheckoutCart] = useState([]) //holds and object that contains the product info in the cart and its quantity
-    const [totalPrice, setTotalPrice]=useState(0)  
-
+    const [totalPrice, setTotalPrice] = useState(0)
 
 
     const handleGetItemById = async (productId) => { //Create a cart in the front end using an object. If item id exists then upload quantity. Else, insert id, product info and total quantity.
@@ -39,32 +59,99 @@ const CheckoutCart = () => {
 
     useEffect(() => {
         for (let i = 0; i < productIds.length; i++) {
-            handleGetItemById(handleGetItemById(productIds[i]));
+            handleGetItemById(productIds[i;
         }
     }, [totalProductQty]) //Update the checkout when add 1 more quantity mimicking ComponentDidUpdate. 
     //Continous loop but slowly. Need to Change.
 
 
 
-
     return (
 
-        <div className="checkoutCart-stage">
+
+        <div style={{
+            margin: "35px",
+            padding: "23px",
+            margin: '32px',
+
+        }} className={classes.root} >
             <p>CHECKOUT  </p>
-            {checkoutCart.length > 0 ? (<div>
-                {checkoutCart.map((item) => {
-                    return (
-                        <div>
-                            <p>{item.productQty}</p>
-                            <img src={item.imgUrl} height="100px" />
-                            <p> {item.productName}</p>
-                            <p>${item.price}</p>
-                        </div>
-                    )
-                })}
-            </div>) : (<div></div>)}
-            <p>TOTAL: {totalPrice}</p>
+            <Grid container spacing={4}>
+
+                <Grid item xs={7} sm={3}>
+                    <Paper className={classes.paper}
+                        style={{
+                            boxShadow: " 1px 1px 1px white",
+                            color: "black",
+                        }}
+                    >
+
+                        {checkoutCart.length > 0 ? (<div>
+                            {checkoutCart.map((item) => {
+
+                                return (
+
+
+                                    <div>
+                                        <img src={item.imgUrl} height="300px" />
+                                        <p> {item.productName}</p>
+                                        <p>${item.price}</p>
+                                        <p>Qty:{item.productQty}</p>
+                                    </div>
+
+                                )
+                            })}
+                        </div>) : (<div></div>)}
+                    </Paper>
+                </Grid>
+                <Grid item xs={3} sm={5}>
+                    <Paper className={classes.paper} style={{
+                        boxShadow: " 1px 1px 1px white",
+                    }}
+                    ></Paper>
+                </Grid>
+            </Grid>
+
+
+
+
+            <Grid container
+                spacing={4}
+            >
+
+                <Grid item xs={7} sm={3}>
+                    <Paper className={classes.paper}
+                        style={{
+                            boxShadow: " 1px 1px 1px white",
+                        }}>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3} sm={5}
+
+                >
+                    <Paper className={classes.paper}
+                        style={{
+                            boxShadow: " 1px 1px 1px white",
+                            color: "black",
+                        }} >
+                        {totalPrice !== 0 ? (<div style={{
+                            border: '1px solid #eed7c1',
+                        }}
+                        > <p>TOTAL: {totalPrice}</p></div>) : (<div></div>)}
+
+
+                    </Paper>
+                </Grid>
+            </Grid>
+
+
+
         </div>
+        // <div className="checkoutCart-stage">
+        //   
+        //
+        //     
+        // </div>
     )
 }
 
