eec039c1a12b8c923b465f26600686593d1b486b
KathyPuma <kathypuma96@gmail.com>
2020-04-19 00:35:48 -0400

    Renamed variables

--- client/src/Components/CheckoutCart.jsx
+++ client/src/Components/CheckoutCart.jsx
@@ -1,36 +1,36 @@
+
 import React, { useState, useEffect, useContext, useRef } from 'react'
 import { Button, InputLabel, MenuItem, Select } from '@material-ui/core'
 import { ItemDetailsContext } from '../Contexts/ItemDetailsContexts'
 import axios from 'axios'
 
 const CheckoutCart = () => {
-    const { updateId, itemId, itemIds, qty, updateQty, totalQty } = useContext(ItemDetailsContext)
-    //Acts like ItemDetailsContext.consumer but allows the entire CheckoutCart.jsx access to the state from Contexts/ItemDeatilsContext.js. 
+    const {productId,  productIds, productQty, totalProductQty } = useContext(ItemDetailsContext)    //Acts like ItemDetailsContext.consumer but allows the entire CheckoutCart.jsx access to the state from Contexts/ItemDeatilsContext.js. 
     // ItemDetailsContext.consumer is found in the return and wraps around html tags (div, p, h1 etc.). It will only give those specific tags access to the state
 
 
-    const [iteminfo, setItemInfo] = useState([]) //holds and object that contains the product info in the cart and its quantity
+    const [checkoutCart, setCheckoutCart] = useState([]) //holds and object that contains the product info in the cart and its quantity
+    const [totalPrice, setTotalPrice]=useState(0)  
 
 
 
-    // const [checkout, setCheckout] = useState([])
-
-
-    const handleGetItemById = async (itemId) => { //Create a cart in the front end using an object. If item id exists then upload quantity. Else, insert id, product info and total quantity.
-        const itemInfo = await axios.get(`/products/images/${itemId}`)
+    const handleGetItemById = async (productId) => { //Create a cart in the front end using an object. If item id exists then upload quantity. Else, insert id, product info and total quantity.
+        const itemInfo = await axios.get(`/products/images/${productId}`)
         let payload = itemInfo.data.payload
         let cartObj = []
-        if (!cartObj[itemId]) {
+        if (!cartObj[productId]) {
             cartObj = {
-                itemId: itemId,
+                productId: productId,
                 productName: payload.product_name,
                 imgUrl: payload.product_image_url,
                 price: payload.product_price,
-                qty: totalQty
+                productQty: totalProductQty
             }
-            setItemInfo([cartObj])
+            setCheckoutCart([cartObj])
+            setTotalPrice(totalPrice + (cartObj.productQty * cartObj.price))
         } else {
-            cartObj[itemId] = totalQty
+            cartObj[productId] = totalProductQty
+            setTotalPrice(totalPrice + (totalProductQty * cartObj.price))
 
         }
     }
@@ -38,10 +38,10 @@ const CheckoutCart = () => {
 
 
     useEffect(() => {
-        for (let i = 0; i < itemIds.length; i++) {
-            handleGetItemById(handleGetItemById(itemIds[i]));
+        for (let i = 0; i < productIds.length; i++) {
+            handleGetItemById(handleGetItemById(productIds[i]));
         }
-    }, [totalQty]) //Update the checkout when add 1 more quantity mimicking ComponentDidUpdate. 
+    }, [totalProductQty]) //Update the checkout when add 1 more quantity mimicking ComponentDidUpdate. 
     //Continous loop but slowly. Need to Change.
 
 
@@ -51,11 +51,11 @@ const CheckoutCart = () => {
 
         <div className="checkoutCart-stage">
             <p>CHECKOUT  </p>
-            {iteminfo.length > 0 ? (<div>
-                {iteminfo.map((item) => {
+            {checkoutCart.length > 0 ? (<div>
+                {checkoutCart.map((item) => {
                     return (
                         <div>
-                            <p>{item.qty}</p>
+                            <p>{item.productQty}</p>
                             <img src={item.imgUrl} height="100px" />
                             <p> {item.productName}</p>
                             <p>${item.price}</p>
@@ -63,9 +63,12 @@ const CheckoutCart = () => {
                     )
                 })}
             </div>) : (<div></div>)}
+            <p>TOTAL: {totalPrice}</p>
         </div>
-
     )
 }
 
 export default CheckoutCart
+
+
+
