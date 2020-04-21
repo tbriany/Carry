21f1180417a7a4de7411d5343bba0f72eff9083a
KathyPuma <kathypuma96@gmail.com>
2020-04-21 17:08:45 -0400

    Create a new route for the checkout cart

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import Landing from "./Components/Landing";
 import Navbar from './Components/NavBar';
 import Checkout from './Components/Checkout';
+
 import ItemPopUp from './Components/ItemPopUp'
 import CheckoutCart from './Components/CheckoutCart'
 import ItemDetailsContextProvider from './Contexts/ItemDetailsContexts'
@@ -13,17 +14,18 @@ function App() {
     <div className="App">
       <ItemDetailsContextProvider>
         <Navbar />
-      </ItemDetailsContextProvider>
+      {/* </ItemDetailsContextProvider> */}
 
       {/* <Landing/> */}
       {/* <p>App.js</p> */}
       <Switch>
-        <ItemDetailsContextProvider>
+        {/* <ItemDetailsContextProvider> */}
           <Route exact path="/" component={Landing} />
           <Route exact path="/checkout" component={Checkout} />
           <Route exact path="/popup" component={ItemPopUp} />
-        </ItemDetailsContextProvider>
+ 
       </Switch>
+      </ItemDetailsContextProvider>
       </div>
       );
     }
