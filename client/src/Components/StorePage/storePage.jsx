5ef8d1531ac3d721c94137aea5e1ea236284a939
aransagarcia <aransagarcia@pursuit.org>
2020-04-22 18:14:24 -0400

    create function to get all stores by id

--- client/src/Components/StorePage/storePage.jsx
+++ client/src/Components/StorePage/storePage.jsx
@@ -1,38 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SingleLineGridList from "./gridList";
 
-import SingleLineGridList from './gridList';
+function StorePage(props) {
+  const [store, setStore] = useState([]);
 
-function StorePage() {
+  useEffect(() => {
+    axios
+      .get(`/stores/${props.match.params.id}`)
+      .then((result) => {
+        console.log(result);
+        setStore(result.data.payload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="StorePage">
-      <div className="Banner" id="banner" style={{position: "relative", height: "40vh", width: "100vw"}}>
-      <img alt="clothes" src="https://boutiquestoredesign.com/wp-content/uploads/2018/09/fashion-retail-womens-clothing-stores-design-ideas-layout-6.jpg" style={{height: "100%", width: "100%"}}></img>
-       <div className="StoreInfo" id="storeInfo" style={{position: "absolute", top: "30%", width: "30%", backgroundColor: "white"}}>
-        <p>Store Name</p>
-        <p>Description</p>
-        <p>Estimated Delivery time || Delivery fee</p>
-        <p>Address</p>
+      <div
+        className="Banner"
+        id="banner"
+        style={{ position: "relative", height: "40vh", width: "100vw" }}
+      >
+        <img
+          alt="clothes"
+          src="https://boutiquestoredesign.com/wp-content/uploads/2018/09/fashion-retail-womens-clothing-stores-design-ideas-layout-6.jpg"
+          style={{ height: "100%", width: "100%" }}
+        ></img>
+        <div
+          className="StoreInfo"
+          id="storeInfo"
+          style={{
+            position: "absolute",
+            top: "30%",
+            width: "30%",
+            backgroundColor: "white",
+          }}
+        >
+          <p>{store.store_name}</p>
+          <p>Description</p>
+          <p>Estimated Delivery time || Delivery fee</p>
+          <p>{store.address}</p>
         </div>
       </div>
       <br></br>
       <div>
-      <SingleLineGridList
-        listTitle="Shop By Category"
-      />
+        <SingleLineGridList listTitle="Shop By Category" />
       </div>
       <div>
-      <SingleLineGridList
-        listTitle="Shop By Type"
-      />
+        <SingleLineGridList listTitle="Shop By Type" />
       </div>
       <div>
-      <SingleLineGridList
-        listTitle="New Arrivals"
-      />
+        <SingleLineGridList listTitle="New Arrivals" />
       </div>
     </div>
   );
 }
-
 
 export default StorePage;
