97d31337020b194add2d8996480a7d546528bec6
aransagarcia <aransagarcia@pursuit.org>
2020-04-28 21:00:48 -0400

    Add more categories (home Decor, Kids, and Health& Wellness

--- client/src/Components/LandingPageComponents/LandingPagecategories.jsx
+++ client/src/Components/LandingPageComponents/LandingPagecategories.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
@@ -14,13 +14,13 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-around",
     overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
-    spacing: "4px",
+    boxSizing: "border-box",
   },
   gridList: {
     flexWrap: "nowrap",
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     transform: "translateZ(0)",
-    spacing: "10",
+    spacing: "20px",
   },
   title: {
     color: theme.palette.primary.light,
@@ -29,6 +29,18 @@ const useStyles = makeStyles((theme) => ({
     background: "white",
     textAlign: "center",
     // 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  imgFullHeight: {
+    height: "100%",
+    transform: "translateX(-50%)",
+    position: "relative",
+    left: "50%",
+  },
+  imgFullWidth: {
+    width: "100%",
+    position: "relative",
+    transform: "translateY(-50%)",
+    top: "50%",
   },
 }));
 
@@ -40,14 +52,14 @@ export default function Categories() {
       id: 1,
       img:
         "https://cdn2.vectorstock.com/i/1000x1000/90/16/fashion-women-in-checkered-dress-drawing-outline-vector-27759016.jpg",
-      title: "Women's",
+      title: "Women",
       author: "author",
     },
     {
       id: 2,
       img:
         "https://thumbs.dreamstime.com/b/fashion-man-men-sketches-white-background-autumn-127439059.jpg",
-      title: "Men's",
+      title: "Men",
       author: "author",
     },
     {
@@ -57,23 +69,48 @@ export default function Categories() {
       title: "Beauty",
       author: "author",
     },
-    { id: 4,
+    {
+      id: 4,
       img:
         "https://www.thestatesman.com/wp-content/uploads/2017/12/accessories.jpg",
       title: "Accessories",
       author: "author",
     },
+    {
+      id: 4,
+      img:
+        "https://www.thestatesman.com/wp-content/uploads/2017/12/accessories.jpg",
+      title: "Home Decor ",
+      author: "author",
+    },
+    {id: 4,
+      img:
+        "https://www.thestatesman.com/wp-content/uploads/2017/12/accessories.jpg",
+      title: "Health & Wellness",
+      author: "author",
+    },
+    {id: 4,
+      img:
+        "https://www.thestatesman.com/wp-content/uploads/2017/12/accessories.jpg",
+      title: "Kids",
+      author: "author",
+    }
   ];
 
   return (
     <div className={classes.root}>
       <GridList className={classes.gridList} cols={3.5}>
         {categories.map((tile) => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
-            <Link to ={`/categories/${tile.id}`}>
+          <GridListTile spacing={"100px"} cellHeight={"1px"} key={tile.img}>
+            <img
+              style={{ boderRaduius: "50px" }}
+              src={tile.img}
+              alt={tile.title}
+            />
+            <Link to={`/categories/${tile.id}`}>
               <GridListTileBar
                 title={tile.title}
+                s
                 classes={{
                   root: classes.titleBar,
                   title: classes.title,
@@ -82,7 +119,6 @@ export default function Categories() {
                 //   <IconButton aria-label={`Heart ${tile.title}`}>
                 //     <StarBorderIcon className={classes.title} />
                 //   </IconButton>}
-                
               />
             </Link>
           </GridListTile>
