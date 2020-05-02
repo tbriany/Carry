f7876974c883cb10daf1e276728435049282cfd7
KathyPuma <kathypuma96@gmail.com>
2020-05-02 09:21:08 -0400

    Update context to update the cart

--- client/src/Contexts/ItemDetailsContexts.js
+++ client/src/Contexts/ItemDetailsContexts.js
@@ -10,49 +10,17 @@ const ItemDetailsContextProvider = (props) => {
     const [productIds, setproductsIds] = useState([]); //Array contains all ids that was added to the checkout bag.
     const [productQty, setProductQty] = useState(0);
     const [totalProductQty, setTotalProductQty] = useState(0); //Total of Product Qty from the itempopup page
-    const [productPrice, setProductPrice] = useState(0);
     const [productSize, setProductSize] = useState('');
-
     const [checkoutCart, setCheckoutCart] = useState([]);
-
-
 
 
     const updateProductQty = (newQty) => {//function the updates the quantity 
         setProductQty(newQty)
     };
 
-    const getProductPrice = (currProdPrice) => {
-        setProductPrice(currProdPrice)
-    };
-
     const getProductSize = (getProductSize) => {
         setProductSize(getProductSize)
     };
-
-
-
-    const addToCart = async () => {
-        if (productQty !== 0 && productSize !== '') {
-            setTotalProductQty(productQty)
-            if (!productIds.includes(productId)) {
-                setproductsIds([...productIds, productId])
-            }
-            try {
-                await axios.post('/checkoutCart/add', { product_id: productId, size: productSize, quantity: productQty, totalPrice: 100 })
-            } catch (err) {
-                console.log("ERROR", err)
-            }
-            setProductQty(0)
-        } else {
-            alert("Missing Input")
-        }
-
-
-
-
-    }
-
 
 
     useEffect(() => {
@@ -62,14 +30,47 @@ const ItemDetailsContextProvider = (props) => {
     const getCheckout = async () => {
         let getCart = await axios.get(`/checkoutCart`)
         setCheckoutCart(getCart.data.payload)
-        console.log(checkoutCart, "checkoutCart")
-
     };
+
+
+    const addToCart = async () => {
+        if (productQty !== 0 && productSize !== '') {
+            setTotalProductQty(productQty)
+            if (!productIds.includes(productId)) {
+                setproductsIds([...productIds, productId])
+            }
+            let productExistInCart = checkoutCart.find(cart => parseInt(productId) === parseInt(cart.product_id))
+            if (!productExistInCart) {
+
+                try {
+                    await axios.post('/checkoutCart/add', { product_id: productId, size: productSize, quantity: productQty })
+                } catch (err) {
+                    console.log("ERROR", err)
+                }
+            } else {
+
+                let prevQty = parseInt(productExistInCart.cartquantity)
+                let current = parseInt(productQty)
+                let totalQty = prevQty + current
+
+                try {
+                    await axios.patch(`/checkoutCart/edit/${productExistInCart.checkoutcart_id}`, { checkoutCart_id: productExistInCart.checkoutcart_id, product_id: productId, size: productSize, quantity: totalQty })
+
+                } catch (err) {
+                    console.log("ERROR", err)
+                }
+            }
+            setProductQty(0)
+        } else {
+            alert("Missing Input")
+        }
+    }
+
 
 
     return (
         // Provider accepts a value containting state and functions. This allows the components access to the state but it must be descendants of the provider.
-        <ItemDetailsContext.Provider value={{ getCheckout, updateProductQty, checkoutCart, productId, productIds, productQty, totalProductQty, getProductPrice, productPrice, getProductSize, productSize, addToCart }}>
+        <ItemDetailsContext.Provider value={{ getCheckout, updateProductQty, checkoutCart, productId, productIds, productQty, totalProductQty, getProductSize, productSize, addToCart }}>
             {props.children}
         </ItemDetailsContext.Provider>
     );
