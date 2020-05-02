abf94befa9d745ab7ac5a3831e866fd8c6227702
giselle <52139642+gsanchez13@users.noreply.github.com>
2020-05-01 20:40:42 -0400

    Update storePage.jsx

--- client/src/Components/StorePage/storePage.jsx
+++ client/src/Components/StorePage/storePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Banner from './storeBanner';
 import CategoriesGridList from "./GridList/categoryGridList";
