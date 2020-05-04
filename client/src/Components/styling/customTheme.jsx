9bd1d498e2ad04062ffb209f5581ccd1c25878d7
gsanchez13 <gisellesanchez@pursuit.org>
2020-05-04 17:18:26 -0400

    adds neutral color codes to custom theme export

--- client/src/Components/styling/customTheme.jsx
+++ client/src/Components/styling/customTheme.jsx
@@ -1,103 +1,26 @@
-import { deepmerge } from '@material-ui/utils';
-import createBreakpoints from './createBreakpoints';
-import createMixins from './createMixins';
-import createPalette from './createPalette';
-import createTypography from './createTypography';
-import shadows from './shadows';
-import shape from './shape';
-import createSpacing from './createSpacing';
-import transitions from './transitions';
-import zIndex from './zIndex';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-function createMuiTheme(options = {}, ...args) {
-  const {
-    breakpoints: breakpointsInput = {},
-    mixins: mixinsInput = {},
-    palette: paletteInput = {},
-    spacing: spacingInput,
-    typography: typographyInput = {},
-    ...other
-  } = options;
-
-  const palette = createPalette(paletteInput);
-  const breakpoints = createBreakpoints(breakpointsInput);
-  const spacing = createSpacing(spacingInput);
-
-  let muiTheme = deepmerge(
-    {
-      breakpoints,
-      direction: 'ltr',
-      mixins: createMixins(breakpoints, spacing, mixinsInput),
-      overrides: {}, // Inject custom styles
-      palette,
-      props: {}, // Provide default props
-      shadows,
-      typography: createTypography(palette, typographyInput),
-      spacing,
-      shape,
-      transitions,
-      zIndex,
+const customTheme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#FAEBD7',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
     },
-    other,
-  );
-
-  muiTheme = args.reduce((acc, argument) => deepmerge(acc, argument), muiTheme);
-
-  if (process.env.NODE_ENV !== 'production') {
-    const pseudoClasses = [
-      'checked',
-      'disabled',
-      'error',
-      'focused',
-      'focusVisible',
-      'required',
-      'expanded',
-      'selected',
-    ];
-    const traverse = (node, parentKey, depth = 1) => {
-      let key;
-
-      // eslint-disable-next-line guard-for-in, no-restricted-syntax
-      for (key in node) {
-        const child = node[key];
-        if (depth === 1) {
-          if (key.indexOf('Mui') === 0 && child) {
-            traverse(child, key, depth + 1);
-          }
-        } else if (pseudoClasses.indexOf(key) !== -1 && Object.keys(child).length > 0) {
-          if (process.env.NODE_ENV !== 'production') {
-            console.error(
-              [
-                `Material-UI: the \`${parentKey}\` component increases ` +
-                  `the CSS specificity of the \`${key}\` internal state.`,
-                'You can not override it like this: ',
-                JSON.stringify(node, null, 2),
-                '',
-                'Instead, you need to use the $ruleName syntax:',
-                JSON.stringify(
-                  {
-                    root: {
-                      [`&$${key}`]: child,
-                    },
-                  },
-                  null,
-                  2,
-                ),
-                '',
-                'https://material-ui.com/r/pseudo-classes-guide',
-              ].join('\n'),
-            );
-          }
-          // Remove the style to prevent global conflicts.
-          node[key] = {};
-        }
-      }
-    };
-
-    traverse(muiTheme.overrides);
-  }
-
-  return muiTheme;
-}
-
-export default createMuiTheme;
+    secondary: {
+      light: '#CD853F',
+      main: '#DEB887',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+});
+export default customTheme
