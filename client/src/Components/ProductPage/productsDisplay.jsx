a51bdaafe38ecd58f6db9181549fc7d582a5c538
tbriany <brianytaveras@pursuit.org>
2020-05-04 22:59:52 -0400

    makes server request to retrive products based on current category name and store id and adds link to popup page

--- client/src/Components/ProductPage/productsDisplay.jsx
+++ client/src/Components/ProductPage/productsDisplay.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -14,72 +15,38 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const gridData = [
-    {
-        img: 'https://www.marni.com/12/12386489MT_13_n_r.jpg',
-        name: 'Product Name',
-        price: '$19'
-    }, 
-    {
-        img: 'https://nationalpostcom.files.wordpress.com/2018/07/tee1.jpg?quality=80&strip=all',
-        name: 'Product Name',
-        price: '$19'
-    }, 
-    {
-        img: 'https://images.express.com/is/image/expressfashion/0036_05051421_1695?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon',
-        name: 'Product Name',
-        price: '$19'
-
-    }, 
-    {
-        img: 'https://cdn.shopify.com/s/files/1/0082/3558/1504/products/lima-sagrada-small-backpack-black_606x606_b48c910d-33ac-46ce-aeb0-8bb3f7b73f7f_1296x.jpg',
-        name: 'Product Name',
-        price: '$19'
-    },
-    {
-        img: 'https://www.marni.com/12/12386489MT_13_n_r.jpg',
-        name: 'Product Name',
-        price: '$19'
-    }, 
-    {
-        img: 'https://nationalpostcom.files.wordpress.com/2018/07/tee1.jpg?quality=80&strip=all',
-        name: 'Product Name',
-        price: '$19'
-    }, 
-    {
-        img: 'https://images.express.com/is/image/expressfashion/0036_05051421_1695?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon',
-        name: 'Product Name',
-        price: '$19'
-    }, 
-    {
-        img: 'https://nationalpostcom.files.wordpress.com/2018/07/tee1.jpg?quality=80&strip=all',
-        name: 'Product Name',
-        price: '$19'
-    },
-    {
-        img: 'https://cdn.shopify.com/s/files/1/0082/3558/1504/products/lima-sagrada-small-backpack-black_606x606_b48c910d-33ac-46ce-aeb0-8bb3f7b73f7f_1296x.jpg',
-        name: 'Product Name',
-        price: '$19'
-    }
-]
-
-
-export default function ProductsDisplay() {
+export default function ProductsDisplay(props) {
   const classes = useStyles();
+
+  const [products, setProducts] = useState([])
+
+  useEffect(async () => {
+  try {
+      const res = await axios.get(`/products/category/${props.categoryName}/${props.storeId}`)
+      setProducts(res.data.payload);
+  } catch (error) {
+      setProducts([])
+      console.log(error);
+  }
+}, [])
+
+console.log(products)
 
   return (
     <div className={classes.root} style={{margin: '20px', padding: '15px'}}>
     <Grid container className={classes.root} justify='center'>
       <Grid item md={10}>
         <Grid container justify="center" spacing={5}>
-          {gridData.map((value) => (
+          {products.map((value) => (
+                <Link to={`/popup/${value.product_id}`}>
                 <Grid key={value} item>
-                    <img alt="backpack" src={value.img} className={classes.image}></img>
+                    <img alt="backpack" src={value.product_image_url} className={classes.image}></img>
                 <Grid item>
-                    <Typography gutterBottom variant="subtitle1">{value.name}</Typography>
-                    <Typography variant="subtitle1">{value.price}</Typography>
+                    <Typography gutterBottom variant="subtitle1">{value.product_name}</Typography>
+                    <Typography variant="subtitle1">$ {value.product_price}</Typography>
                 </Grid>
                 </Grid>
+                </Link>
           ))}
         </Grid>
       </Grid>
