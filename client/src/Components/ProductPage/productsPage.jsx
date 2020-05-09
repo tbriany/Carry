84b436308dce32e061f7c11889e914cc0f7e8b82
KathyPuma <kathypuma96@gmail.com>
2020-05-09 09:45:26 -0400

    Import context and passed functions to ProductsDisplay page

--- client/src/Components/ProductPage/productsPage.jsx
+++ client/src/Components/ProductPage/productsPage.jsx
@@ -1,26 +1,31 @@
-import React, { useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { Button, InputLabel, MenuItem, Select } from '@material-ui/core';
+import { ItemDetailsContext } from '../../Contexts/ItemDetailsContexts';
+import axios from 'axios';
 import Banner from '../StorePage/storeBanner';
 import MultipleSelect from '../StorePage/filterForm';
 import ProductsDisplay from '../ProductPage/productsDisplay';
 
+
+
 function ProductsPage(props) {
-
-
+  const { getProductId, productId } = useContext(ItemDetailsContext);
   return (
-    <div className="ProductsPage" style={{textAlign: "center"}}>
+
+    <div className="ProductsPage" style={{ textAlign: "center" }}>
       <Banner
-      storeId={props.match.params.id}
-       />
-      <MultipleSelect/>
+        storeId={props.match.params.id}
+      />
+      <MultipleSelect />
       <br></br>
       <ProductsDisplay
-      storeId={props.match.params.id}
-      categoryName={props.match.params.category_name}
+        storeId={props.match.params.id}
+        categoryName={props.match.params.category_name}
+        getProductId={getProductId}
+        currentProduId={productId}
       />
     </div>
   );
 }
-
 
 export default ProductsPage;
