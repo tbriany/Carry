931f9fcb0f0ef49b27ac5bb2356ef9d373c1f5f5
aransagarcia <aransagarcia@pursuit.org>
2020-05-14 02:17:19 -0400

    In categoryPage passed array of products as a prop to component CategoryGridList. Added some comments for remainders. Display Grid on category Page with images of products of specific category

--- client/src/Components/LandingPageComponents/CategoryPage.jsx
+++ client/src/Components/LandingPageComponents/CategoryPage.jsx
@@ -6,50 +6,53 @@ import { CheckBoxOutlineBlankOutlined } from "@material-ui/icons";
 //import TitlebarGridList from './CategoryGrid'
 
 const CategoryPage = (props) => {
-  const [products, setProducts] = useState('')
+  const [products, setProducts] = useState([]);
 
+  // Ask team was unable to store category_id using hooks.
   // const [category_id, setCategory_id] =useState(0)
-
   // setCategory_id(props.match.params.type)
-  
- 
-  const category_id = props.match.params.type
 
-  console.log('categoryPage category_id',category_id)
+  const category_id = props.match.params.type;
+  console.log("categoryPage category_id", category_id);
+
   // useeffect to  make a network request to backend to get all stores/ products of that category.
-
-  // console.log(category_id)
-
-
+  // Ask Adam or team why this is being rendered twice.
   useEffect(() => {
     async function fetchData() {
-        try {
-            const res = await axios.get(`/products/category/${category_id}`)
-            setProducts(res.data.payload);
-            console.log(res.data.payload)
-        } catch (error) {
-            setProducts([])
-            console.log(error);
-        }
+      try {
+        const res = await axios.get(`/products/category/${category_id}`);
+        setProducts(res.data.payload);
+        console.log(res.data.payload);
+      } catch (error) {
+        setProducts([]);
+        console.log(error);
+      }
     }
-  fetchData()
-}, [category_id])
+    fetchData();
+  }, [category_id]);
 
-console.log('products from category name', products)
+  console.log("products on Category Page", products);
   return (
     <div className="CategoryPage">
-      <h1> Category Page in the making</h1>
+      <div>
+        <h1> {category_id}</h1>
+        <div className="SearchBar">
+          {" "}
+          <p> Here will be a search bar.</p>{" "}
+        </div>
+      </div>
+
       <div className="Content">
         <div>
-          <CategoryGridList categoryId={props.categoryId}
-          // product_name = {products.}
-           />
+          <CategoryGridList
+            categoryId={props.categoryId}
+            product_name={products.product_name}
+            products={products}
+          />
         </div>
       </div>
     </div>
   );
 };
-
-
 
 export default CategoryPage;
