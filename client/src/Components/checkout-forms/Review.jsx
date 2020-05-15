c982993f0e02a733ac7d050fdfb3e4c764fa360c
KathyPuma <kathypuma96@gmail.com>
2020-05-14 22:06:32 -0400

    Add options for delivery

--- client/src/Components/checkout-forms/Review.jsx
+++ client/src/Components/checkout-forms/Review.jsx
@@ -1,11 +1,40 @@
 import React, { useState, Fragment } from 'react';
-import { TextField } from '@material-ui/core';
+// import { TextField } from '@material-ui/core';
 
 const Review = () => {
     return (
-        <Fragment>
-            <TextField required variant='standard' label='Review'></TextField>
-        </Fragment>
+
+        <div >
+            <div className='shipping' style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                <input type="radio" className='rush' name='shipping'>
+                </input>
+                <label htmlFor="rush">Rush Delivery: $15
+ <p>Recieve Items in less than 3 hours</p>
+                </label>
+            </div>
+
+            <div className='shipping' style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                <input type="radio" className='standard' name='shipping'>
+                </input>
+                <label htmlFor="standard">End of Day Delivery: $10
+ <p>Recieve Items by the 9pm</p>
+                </label>
+            </div>
+
+            <div className='shipping' style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                <input type="radio" className='standard' name='shipping'>
+                </input>
+                <label htmlFor="standard">Next Day Delivery: $5
+ <p>Recieve items tomorrow</p>
+                </label>
+            </div>
+
+
+
+
+
+        </div>
+
     )
 };
 
