c374c652c1c3ef0820778b084d31a3e8c1acf697
KathyPuma <kathypuma96@gmail.com>
2020-05-16 00:59:12 -0400

    Move styling to another page

---
+++ client/src/Components/styling/sidePopTheme.jsx
@@ -0,0 +1,18 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+export  const sidePopUp = makeStyles({
+    list: {
+      width:'100%',
+    },
+    fullList: {
+      width: "50%",
+    },
+    paperAnchorRight:{
+      width: "45%",
+      display: 'flex',
+      alignItems: 'center',
+    },
+  
+  });
+  
+  
