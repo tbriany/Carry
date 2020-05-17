069e743b59b7b500a6daac8a9c6f6be41b587cea
tbriany <brianytaveras@pursuit.org>
2020-05-17 03:53:04 -0400

    adds navbar to product page

--- client/src/Components/ProductPage/productsPage.jsx
+++ client/src/Components/ProductPage/productsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, InputLabel, MenuItem, Select } from '@material-ui/core';
 import { ItemDetailsContext } from '../../Contexts/ItemDetailsContexts';
 import axios from 'axios';
@@ -11,15 +12,27 @@ import ProductsDisplay from '../ProductPage/productsDisplay';
 function ProductsPage(props) {
   const { getProductId, productId } = useContext(ItemDetailsContext);
   const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([]);
 
   const categoryName = props.match.params.category_name
   const storeId = props.match.params.id
+
+  // const fetchCategories = async () => {
+  //   try {
+  //     const categories = await axios.get(`/products/categories/all`)
+  //     setCategories(categories.data.payload)
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   useEffect(() => {
     async function fetchData() {
         try {
             const res = await axios.get(`/products/category/${categoryName}/${storeId}`)
+            const categories = await axios.get(`/products/categories/all`)
             setProducts(res.data.payload);
+            setCategories(categories.data.payload);
         } catch (error) {
             setProducts([])
             console.log(error);
@@ -28,14 +41,42 @@ function ProductsPage(props) {
   fetchData()
 }, [categoryName, storeId])
 
+
+
   return (
 
     <div className="ProductsPage" style={{ textAlign: "center" }}>
       <Banner
         storeId={props.match.params.id}
       />
+
+    <div className ='CategoryNav'
+     style= {{
+       display:'flex', 
+       justifyContent: 'space-evenly', 
+       marginLeft:'20px', 
+       marginRight: '30px',
+       marginBottom: '20px',
+     }}>
+       {categories.map((value) => (<Link to = {`/store/${storeId}/${value.category_name}`}
+       
+       style ={{textDecoration:'none', color: '#CD853F', active:'#FAEBD7'}}
+
+      >  {value.category_name}
+       </Link>))}
+     </div>
+
       <MultipleSelect />
       <br></br>
+      <h2
+              style={{
+                fontFamily: "Palatino Linotype",
+                // color: "black",
+                color: "#CD853F",
+              }}
+            >
+              {props.match.params.category_name}
+            </h2>
       <ProductsDisplay
         getProductId={getProductId}
         currentProduId={productId}
