e9e4723ecbe444cf27c75e15a056a860366ab341
KathyPuma <kathypuma96@gmail.com>
2020-05-26 00:41:14 -0400

    Fixed popup to fit under the navbar

--- client/src/Components/styling/sidePopTheme.jsx
+++ client/src/Components/styling/sidePopTheme.jsx
@@ -1,22 +1,22 @@
-
 import { makeStyles } from '@material-ui/core/styles';
-export  const sidePopUp = makeStyles({
-    list: {
-      width:'100%',
-    },
-    fullList: {
-      width: "50%",
-    },
-    paperAnchorRight:{
-      width: "55%",
-      display: 'flex',
-      alignItems: 'center',
-      bottom: 'inherit',
-      top: 'auto',
-      height: '92vh',
+export const sidePopUp = makeStyles((theme) => ({
 
+  list: {
+    width: '100%',
+  },
+  fullList: {
+    width: "50%",
+  },
+  paperAnchorRight: {
+    width: "55%",
+    display: 'flex',
+    alignItems: 'center',
+    bottom: 'inherit',
+    height: '92vh',
+    top: '56px',
+    [theme.breakpoints.up('sm')]: {
+      top: '64px',
     },
-  
-  });
-  
-  
+  },
+}));
+
