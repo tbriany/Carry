d38cd9afbd2a62c227c672ac663f874667b78252
KathyPuma <kathypuma96@gmail.com>
2020-06-01 23:46:16 -0400

    Update checkoutcart context by using the updated backend route

--- client/src/Contexts/CheckoutCartContext.js
+++ client/src/Contexts/CheckoutCartContext.js
@@ -8,12 +8,12 @@ const CheckoutCartContextProvider = (props) => {
     const [brandIdInCart, setBrandIdInCart] = useState(0);
     const [brandId, setBrandId] = useState(0);
     const [productId, setProductId] = useState(1);//Current Product id
-    const [productIds, setproductsIds] = useState([]); //Array contains all ids that was added to the checkout bag.
     const [productQty, setProductQty] = useState(0);
     const [totalProductQty, setTotalProductQty] = useState(0); //Total of Product Qty from the itempopup page
     const [productSize, setProductSize] = useState('default');
     const [checkoutCart, setCheckoutCart] = useState([]);
     const [shippingOption, setShippingOption] = useState(15)
+
 
     const updateProductQty = (newQty) => {
         setProductQty(newQty)
@@ -36,39 +36,33 @@ const CheckoutCartContextProvider = (props) => {
     }, [])
 
     const getCheckout = async () => {
-        let getCart = await axios.get(`/checkoutCart`)
-        let getCartPayload = getCart.data.payload
-        setCheckoutCart(getCartPayload)
+        let getCheckoutCart = await axios.get('/checkoutCart/session')
+        let CartPayload = getCheckoutCart.data.payload
+        setCheckoutCart(CartPayload)
 
-        let getAllProductIdFromCart = getCartPayload.map(el => {
-            return el.product_id
-        })
-        setproductsIds(getAllProductIdFromCart)
+
     };
-
-
-
 
     const addToCart = async () => {
         if (productQty !== 0 && productSize !== 'default') {
-            setTotalProductQty(productQty)
-            if (!productIds.includes(productId)) {
+            const productExistInCart = await axios.get(`/checkoutCart/items/productId/${productId}/${productSize}`)
+            const productExistPayload = productExistInCart.data.payload
+            if (checkoutCart.length === 0 || !productExistPayload) {
                 try {
-                    await axios.post('/checkoutCart/add', { product_id: productId, size: productSize, quantity: productQty })
+                    await axios.post('/checkoutCart/items/add', { product_id: productId, size: productSize, quantity: productQty })
                     getCheckout()
                 } catch (err) {
                     console.log("ERROR", err)
                 }
             } else {
+                let updateQty = productExistPayload.cartquantity + parseInt(productQty)
+                console.log(productExistPayload, productExistPayload.product_id, productExistPayload.size)
                 try {
-                    let productInCart = await axios.get(`/checkoutCart/productId/${productId}`)
-                    let productPayload = productInCart.data.payload
-                    let totalQty = parseInt(productPayload.cartquantity) + parseInt(productQty)
-                    handleUpdateQuantity(productPayload.checkoutcart_id, productId, productSize, totalQty)
-                    getCheckout()
+                    await axios.patch(`/checkoutCart/items/edit`, { product_id: productExistPayload.product_id, size: productExistPayload.size, quantity: updateQty })
                 } catch (err) {
                     console.log("ERROR", err)
                 }
+                getCheckout()
             }
             setProductQty(0)
             setProductSize('default')
@@ -76,18 +70,9 @@ const CheckoutCartContextProvider = (props) => {
     }
 
 
-    const handleUpdateQuantity = async (checkoutId, currProductId, currSize, updatedQty) => {
-        console.log('checkoutId', checkoutId)
-        try {
-            await axios.patch(`/checkoutCart/edit/${checkoutId}`, { checkoutCart_id: checkoutId, product_id: currProductId, size: currSize, quantity: updatedQty })
-        } catch (err) {
-            console.log("ERROR", err)
-        }
-    }
-
     return (
         // Provider accepts a value containting state and functions. This allows the components access to the state but it must be descendants of the provider.
-        <CheckoutCartContext.Provider value={{ shippingOption, getShipping, getProductId, getCheckout, updateProductQty, checkoutCart, productId, productIds, productQty, totalProductQty, getProductSize, productSize, addToCart }}>
+        <CheckoutCartContext.Provider value={{ shippingOption, getShipping, getProductId, getCheckout, updateProductQty, checkoutCart, productId, productQty, totalProductQty, getProductSize, productSize, addToCart }}>
             {props.children}
         </CheckoutCartContext.Provider>
     );
