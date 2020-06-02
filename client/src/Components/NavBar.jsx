4eac19755b508980b64cb988010d3443d6c5ab80
KathyPuma <kathypuma96@gmail.com>
2020-06-02 17:25:01 -0400

    Rename imported function

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -13,7 +13,7 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import Popover from '@material-ui/core/Popover';
 import customTheme from './styling/customTheme';
-import CartPopUp from './CartPopUp';
+import CartPopover from './Popover/CartPopover';
 import { Context, Store } from '../Contexts/CustomerContext';
 import ProductsPage from './ProductPage/productsPage';
 import navbarStyles from './styling/navbarStyles';
@@ -212,7 +212,7 @@ function Navbar() {
                 zIndex: 9001, 
               }}
             >
-              <CartPopUp />
+              <CartPopover/>
             </Popover>
 
           </div>
