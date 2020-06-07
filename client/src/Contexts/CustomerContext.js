9829bfb147a4183c2e20e8f8db10f45570cc71ab
gsanchez13 <gisellesanchez@pursuit.org>
2020-06-07 13:13:19 -0400

    reates conditional for landing customer object

--- client/src/Contexts/CustomerContext.js
+++ client/src/Contexts/CustomerContext.js
@@ -4,7 +4,7 @@ import { LoginReducer } from '../Components/Reducers/reducers';
 //listener function checks browser localStorage to see if the key 'customer' exists
 //if it does, it will take the string thats saved under customer, turn it into a JSON object and return that object
 //if not, it will return an empty object that we will use when we modify the state in the store
-const listener = () => {
+const getUser = () => {
     if (window.localStorage.getItem('customer') !== null) {
         return JSON.parse(
             window.localStorage.getItem('customer')
@@ -23,12 +23,17 @@ export const Context = createContext(initialState);
 //-- and an initalState, which is declared previously
 export const Store = ({ children }) => {
     const [state, dispatch] = useReducer(LoginReducer, initialState);
-//the useEEffect hook here takes an object from the listener function that can be either a user that is stored in the LocalStorage or an empty object, meaning there is no user stored / logged in and they are anonymous
+    //the useEEffect hook here takes an object from the getUser function that can be either a user that is stored in the LocalStorage or an empty object, meaning there is no user stored / logged in and they are anonymous
     useEffect(() => {
-        let data = listener();
-        dispatch({ type: 'USER_CLICKED_LOGIN', payload: data });
+        let user = getUser();
+        if (user.email) {
+            dispatch({ type: 'SET_USER', payload: user });
+        }
+        else {
+            dispatch({ type: 'REMOVE_USER', payload: user});
+        }
     }, []);
-//every time the store mounts (since it is a context wrapper, it will mount across our app) the useEffect hook will read the localStorage to see if there is a user logged it or not. it will then modify our state in the store with the action USER_CLICKED_LOGIN and the objet containing user info or empty object
+    //every time the store mounts (since it is a context wrapper, it will mount across our app) the useEffect hook will read the localStorage to see if there is a user logged it or not. it will then modify our state in the store with the action SET_USER and the objet containing user info or empty object
     return (
         <Context.Provider value={[state, dispatch]}>
             {children}
