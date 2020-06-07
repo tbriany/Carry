10e979b44e704779aab7e84bce9210b8e0714d0b
gsanchez13 <gisellesanchez@pursuit.org>
2020-06-07 13:14:33 -0400

    adds inital styling to account component

--- client/src/Components/Account.jsx
+++ client/src/Components/Account.jsx
@@ -1,16 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
 import { Context } from '../Contexts/CustomerContext';
-import TextField from '@material-ui/core/TextField';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import { accountStyles } from './styling/accountStyles';
 
 const Account = () => {
+    const classes = accountStyles();
     const [state, dispatch] = useContext(Context);
+    let user = {...state.user.info};
     return (
-        <div>
-            <Typography>
-                Welcome !
+            <Fragment>
+            <CssBaseline/>
+            <Container maxWidth='sm' className={classes.container}>
+                <Typography variant='h4'className={classes.text}>
+                    Welcome {user.firstname}!
             </Typography>
-        </div>
+            </Container>
+            </Fragment>
     )
 };
 
