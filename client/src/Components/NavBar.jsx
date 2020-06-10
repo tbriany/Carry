651771bd002368ac89a3bae1c8c76d479abc2091
aransagarcia <aransagarcia@pursuit.org>
2020-06-10 16:50:08 -0400

    Replaced CARRY Logo on navbar. Added new carryimage

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -32,7 +32,7 @@ function Navbar() {
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
   const history = useHistory();
-  const logo = require('./carry.png')
+  const logo = require('./carry2.png')
   
 
   const handleProfileMenuOpen = (event) => {
