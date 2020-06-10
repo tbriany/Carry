91b8360d337a747105e0b5a92a5a6309ecdaf2ac
gsanchez13 <gisellesanchez@pursuit.org>
2020-06-10 17:29:22 -0400

    adds nextstep in place order

--- client/src/Components/Checkout.jsx
+++ client/src/Components/Checkout.jsx
@@ -6,9 +6,10 @@ import PaymentForm from './CheckoutForms/PaymentForm';
 import Review from './CheckoutForms/Review';
 import PlaceOrder from './CheckoutForms/PlaceOrder';
 import customTheme from './styling/customTheme';
-import CustomerContext from '../Contexts/CustomerContext';
+import { Context } from '../Contexts/CustomerContext';
 import { checkoutStyles } from './styling/checkoutStyles';
 import sendMessage from './orderMessage';
+import axios from 'axios';
 
 const steps = ['Delivery address', 'Payment method', 'Delivery Terms'];
 const getStepComponent = (step, props) => {
@@ -30,6 +31,7 @@ const getStepComponent = (step, props) => {
 const Checkout = (props) => {
     const classes = checkoutStyles();
     const [activeStep, setActiveStep] = useState(0);
+    const [state, dispatch] = useContext(Context);
     const handleNextStep = () => {
         setActiveStep(activeStep + 1)
     };
@@ -37,7 +39,8 @@ const Checkout = (props) => {
         setActiveStep(activeStep - 1)
     };
     const handlePlaceOrder = () => {
-        sendMessage({orderNum: '12834969823'})
+        handleNextStep();
+        sendMessage({ orderNum: '12834969823' })
     };
     return (
         <Container>
@@ -67,8 +70,11 @@ const Checkout = (props) => {
                                     Back
                                  </Button>
                             )}
-                            <Button 
-                            onClick={activeStep === steps.length - 1 ? handlePlaceOrder : handleNextStep} className={classes.button} variant='contained' color='grey'>
+                            <Button
+                                onClick={activeStep === steps.length - 1 ? handlePlaceOrder : handleNextStep}
+                                className={classes.button}
+                                variant='contained'
+                                color={customTheme.palette.secondary.main}>
                                 {activeStep === steps.length - 1 ? 'Place Order' : 'Next'}
                             </Button>
                         </div>
