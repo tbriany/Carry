0a93a3e5bbb0e3be1ee9d2521b185d1537e5e9ef
KathyPuma <kathypuma96@gmail.com>
2020-06-10 12:59:02 -0400

    Add use effect in checkoutcart context

--- client/src/Contexts/CheckoutCartContext.js
+++ client/src/Contexts/CheckoutCartContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const CheckoutCartContext = createContext();
@@ -6,7 +6,7 @@ export const CheckoutCartContext = createContext();
 const CheckoutCartContextProvider = (props) => {
     const [productId, setProductId] = useState(1);//Current Product id
     const [checkoutCart, setCheckoutCart] = useState([]);
-    const [shippingOption, setShippingOption] = useState(15) 
+    const [shippingOption, setShippingOption] = useState(15)
     const [checkoutCartId, setCheckoutCartId] = useState(null)
     const [qtyInBag, setQtyInBag] = useState(0)
 
@@ -20,14 +20,17 @@ const CheckoutCartContextProvider = (props) => {
     const getCheckoutCartId = (checkoutId) => {
         setCheckoutCartId(checkoutId)
     }
+    useEffect(() => {
+        getCheckout()
+    }, [shippingOption, qtyInBag]);
 
     const getCheckout = async () => {
         let allCheckoutCart = await axios.get('/checkoutCart/session')
         let allCartPayload = allCheckoutCart.data.payload
         setCheckoutCart(allCartPayload)
         let totalItem = 0
-        allCartPayload.map(product =>{
-            totalItem  =  totalItem + product.cartquantity   
+        allCartPayload.map(product => {
+            totalItem = totalItem + product.cartquantity
         })
         setQtyInBag(totalItem)
     };
@@ -36,7 +39,7 @@ const CheckoutCartContextProvider = (props) => {
     return (
         // Provider accepts a value containting state and functions. This allows the components access to the state but it must be descendants of the provider.
         <CheckoutCartContext.Provider value={{
-            shippingOption, getShipping, getProductId, getCheckout, checkoutCart, productId,checkoutCartId,
+            shippingOption, getShipping, getProductId, getCheckout, checkoutCart, productId, checkoutCartId,
             getCheckoutCartId, qtyInBag, setQtyInBag
         }}>
             {props.children}
