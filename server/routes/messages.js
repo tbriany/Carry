c035b9430d144b09ecd2a731c44a6df6accbd579
tbriany <brianytaveras@pursuit.org>
2020-06-11 03:22:20 -0400

    removed console logs

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -10,8 +10,8 @@ router.post('/send', async (req, res, next) => {
   let phoneNum = req.body.number
   let text = req.body.text
 
-  console.log(phoneNum)
-  console.log(text)
+  // console.log(phoneNum)
+  // console.log(text)
 
   try {
     client.messages.create({
@@ -20,7 +20,7 @@ router.post('/send', async (req, res, next) => {
       to: phoneNum
     })
       .then(message => {
-        console.log(message)
+        // console.log(message)
         res.status(200).json({
           status: "success",
           message: 'Text Message Sent',
