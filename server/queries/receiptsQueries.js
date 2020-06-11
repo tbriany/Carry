c07049d9dfdb92be2e93ea7ed2129a1f9d1929a0
KathyPuma <kathypuma96@gmail.com>
2020-06-11 19:47:49 -0400

    Create a query that gets all receipt by orderId

--- server/queries/receiptsQueries.js
+++ server/queries/receiptsQueries.js
@@ -6,6 +6,20 @@ const getAll = async () => {
 }
 
 
+const getreceiptByOrderId = async (order_id) => {
+	const getQuery = `
+	SELECT customers.city, customers.state, customers.zip_code,  customers.address, customers.firstname, customers.firstname,    customers.address, stores.stores_name, stores.store_logo, stores.address AS StoreAddress, stores.city AS storeCity,  stores.state AS storeState, stores.zip_code AS storeZipCode, 
+	orders.order_id, orders.order_status, orders.required_date, orders.time_ordered,  orders.delivery_fee,  orders.total, receipts.reciept
+	FROM orders  
+	JOIN stores ON orders.store_id =stores.store_id
+	JOIN customers ON orders.customer_id =  customers.customer_id
+	JOIN receipts ON orders.receipt_id = receipts.receipt_id 
+	WHERE orders.order_id =$/order_id/
+	`;
+	return await db.any(getQuery, { order_id });
+  };
+
+  
 const addReceipt = async (rec) => {
 	const newReceiptQuery =
 		`
@@ -25,5 +39,6 @@ const getReceiptsByCustomerId = async (customer_id) => {
 module.exports = {
 	addReceipt,
 	getReceiptsByCustomerId,
-	getAll
+	getAll,
+	getreceiptByOrderId
 }
