a1265469105085dfaa97062637668a50ed769c80
KathyPuma <52139614+KathyPuma@users.noreply.github.com>
2020-06-12 23:14:26 -0400

    Move carttotal to context  (#137)
    * Create a global variable to hold the carttotal and update the getCheckout function
    
    * Pass cart total to checkout file
    
    * Remove hardcoded number for total
    
    * Pass cart total and modify handleCart total function

--- client/src/Components/CheckoutCart/CheckoutCartPage.jsx
+++ client/src/Components/CheckoutCart/CheckoutCartPage.jsx
@@ -6,49 +6,48 @@ import "./CheckoutCart.css";
 
 
 const CheckoutCart = () => {
-    const { getCheckout, checkoutCart, shippingOption } = useContext(CheckoutCartContext) //Grab state from context file
-    const [cartTotal, setCartTotal] = useState()
-    const [changeinQty, setchangeinQty] = useState(false)
-
+    const { getCheckout, checkoutCart, shippingOption, cartTotal, setCartTotal , setQtyInBag} = useContext(CheckoutCartContext) //Grab state from context file
 
     useEffect(() => {
-        getCheckout()
-        if (checkoutCart.length > 0) {
-            handleCartTotal()
-        }
-    }, [changeinQty, shippingOption])
-
-
-    const handleDeleteProduct = async (checkoutId) => {
-        try {
-            await axios.delete(`/checkoutCart/items/delete/${checkoutId}`)
-            getCheckout()
-            handleCartTotal()
-        } catch (err) {
-            console.log("ERROR", err)
-        }
-    }
+        handleCartTotal()
+    }, [shippingOption])
 
     const handleCartTotal = async () => {
         try {
             let total = await axios.get(`/checkoutCart/items/checkoutTotal`)
             let checkoutTotal = total.data.payload.checkouttotal
-            let cartTotal = parseInt(checkoutTotal) + parseInt(shippingOption)
-            setCartTotal(cartTotal)
+            if (checkoutTotal === null) {
+                setCartTotal(0)
+                setQtyInBag(0)
+            } else {
+                let cartTotal = parseInt(checkoutTotal) + parseInt(shippingOption)
+                console.log(checkoutTotal , shippingOption)
+                setCartTotal(cartTotal )
+            }
         } catch (err) {
             console.log("ERROR", err)
         }
+    }
+
+
+    const handleDeleteProduct = async (checkoutId) => {
+        try {
+            await axios.delete(`/checkoutCart/items/delete/${checkoutId}`)
+            await handleCartTotal()
+        } catch (err) {
+            console.log("ERROR", err)
+        }
+
     }
 
     const handleIncrementQty = async (checkoutId, prodId, prodSize, currQty) => {
         try {
             let updateQty = parseInt(currQty) + 1
             await axios.patch(`/checkoutCart/items/edit`, { product_id: prodId, size: prodSize, quantity: updateQty })
-            changeinQty ? setchangeinQty(false) : setchangeinQty(true)
+            await handleCartTotal()
         } catch (err) {
             console.log("ERROR", err)
         }
-
     }
 
     const handleDecrementQty = async (checkoutId, prodId, prodSize, currQty) => {
@@ -57,7 +56,7 @@ const CheckoutCart = () => {
             try {
                 let updateQty = parseInt(currQty) - 1
                 await axios.patch(`/checkoutCart/items/edit`, { product_id: prodId, size: prodSize, quantity: updateQty })
-                changeinQty ? setchangeinQty(false) : setchangeinQty(true)
+                await handleCartTotal()
             } catch (err) {
                 console.log("ERROR", err)
             }
@@ -81,3 +80,4 @@ const CheckoutCart = () => {
 }
 
 export default CheckoutCart
+
